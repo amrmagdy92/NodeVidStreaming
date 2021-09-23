334bd234798f22aa8a87d513160ac80335674e9d
Amr Ahmed <amr.magdy.ahmed92@gmail.com>
2021-09-23 22:38:12 +0200

    Added a mongoose data to hold the video meta data

---
+++ server/model/VideoData.js
@@ -0,0 +1,9 @@
+var mongoose = require('mongoose');
+
+var VideoDataSchema = new mongoose.Schema({
+    filePath: { type: String, required: true, unique: true },
+    fileSize: { type: Number, required: true },
+    fileQuality: { type: String, required: true }
+});
+
+module.exports = mongoose.model('video_data', VideoDataSchema);
